ecaa852fa3fdc18a4659c26874713c5e5c1ca4e7
Dhanashree <maskar.dv@gmail.com>
2022-01-27 12:01:03 +0000

    firstcommit

--- test/basic-details.test.js
+++ test/basic-details.test.js
@@ -2,7 +2,29 @@ import { html, fixture, expect } from '@open-wc/testing';
 import { stub } from 'sinon';
 import '../src/LoanBasicDetails/BasicDetails.js';
 
+const basicDetails={
+  name:'home-loan',
+  amount:'10000',
+  period:'5',
+ 
+};
+
+const el=await fixture(html`<basic-details .basicdata=${basicDetails}></basic-details>`);
+
 describe('Basic details', () => {
   // Write test cases inside this block
-  // refer basic-details.js files
+  it('gets the amount', async () => {
+    expect(el.basicdata.name).to.equal('home-loan');
+  });
+  it('gets the amount', async () => {
+    expect(el.basicdata.amount).to.equal('10000');
+  });
+
+  it('gets the periodd', async () => {
+    expect(el.basicdata.period).to.equal('5');
+  });
+  it('gets the period', async () => {
+    expect(el.basicdata.period).to.be.a('string');
+  });
+ 
 });
--- test/headers.test.js
+++ test/headers.test.js
@@ -2,6 +2,20 @@ import { html, fixture, expect } from '@open-wc/testing';
 import { stub } from 'sinon';
 import '../src/header/Header.js';
 
-describe('loan-header', () => {
+
+describe('loan-header...', () => {
   // Write test cases inside this block
+  it("should be accessible", ()=>{
+    expect("true").to.equal("true")
+  })
+});
+describe('loan-header works or not', () => {
+  // Write test cases inside this block
+  it("workss", async()=>{
+    const el=await fixture(html`<loan-header></loan-header>`);
+  })
+});
+it('can semantically compare lightDom trees', async () => {
+  const el = await fixture(`<div><!-- comment --><h1>${'Hey'}  </h1>  </div>`);
+  expect(el).lightDom.to.equal('<h1>Hey</h1>');
 });
