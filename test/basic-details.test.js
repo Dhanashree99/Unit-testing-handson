3c12310e1339f5ba6868c1d1cf7f37bc93362e95
Dhanashree <maskar.dv@gmail.com>
2022-01-28 10:26:12 +0000

    commit2

--- test/basic-details.test.js
+++ test/basic-details.test.js
@@ -2,16 +2,15 @@ import { html, fixture, expect } from '@open-wc/testing';
 import { stub } from 'sinon';
 import '../src/LoanBasicDetails/BasicDetails.js';
 
-const basicDetails={
-  name:'home-loan',
-  amount:'10000',
-  period:'5',
  
-};
 
-const el=await fixture(html`<basic-details .basicdata=${basicDetails}></basic-details>`);
 
+const el   = await fixture(html`<basic-details></basic-details>`);
 describe('Basic details', () => {
+
+   it('Accessible', async() =>{
+     expect(el).to.be.accessible();
+   });
   // Write test cases inside this block
   it('gets the amount', async () => {
     expect(el.basicdata.name).to.equal('home-loan');
